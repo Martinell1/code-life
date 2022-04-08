e10e53103e8ec8f92862dbafc54feb036b00b394
AsheOne <1029129827@qq.com>
2022-04-08 09:36:49 +0800

    Update 429.js

--- leetcode/429.js
+++ leetcode/429.js
@@ -30,10 +30,5 @@
       }
       ans.push(level);
   }
-
   return ans;
 };
-
-root = 1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14
-
-console.log(levelOrder(root));
