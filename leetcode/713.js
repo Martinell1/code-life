4e99154f0020cafb82d0a1ed0536746836e873db
AsheOne <1029129827@qq.com>
2022-05-05 14:42:14 +0800

    Create 713.js

---
+++ leetcode/713.js
@@ -0,0 +1,17 @@
+var numSubarrayProductLessThanK = function(nums, k) {
+    let n = nums.length, ret = 0;
+    let prod = 1, i = 0;
+    for (let j = 0; j < n; j++) {
+        prod *= nums[j];
+        while (i <= j && prod >= k) {
+            prod /= nums[i];
+            i++;
+        }
+        ret += j - i + 1;
+    }
+    return ret;
+};
+
+
+nums = [10,5,2,6], k = 100
+console.log(numSubarrayProductLessThanK(nums,k));
