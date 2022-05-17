14fc1f06842ceece0b4ec556f88f6a6e04e4bd9e
AsheOne <1029129827@qq.com>
2022-05-18 00:14:04 +0800

    update

--- offer/32-II.js
+++ offer/32-II.js
@@ -1,33 +1,22 @@
-var zigzagLevelOrder = function(root) {
+var levelOrder = function(root) {
+    const ret = [];
     if (!root) {
-        return [];
+        return ret;
     }
 
-    const ans = [];
-    const nodeQueue = [root];
-
-    let isOrderLeft = true;
-
-    while (nodeQueue.length) {
-        let levelList = [];
-        const size = nodeQueue.length;
-        for (let i = 0; i < size; ++i) {
-            const node = nodeQueue.shift();
-            if (isOrderLeft) {
-                levelList.push(node.val);
-            } else {
-                levelList.unshift(node.val);
-            }
-            if (node.left !== null) {
-                nodeQueue.push(node.left);
-            }
-            if (node.right !== null) {
-                nodeQueue.push(node.right);
-            }
-        }            
-        ans.push(levelList);
-        isOrderLeft = !isOrderLeft;
+    const q = [];
+    q.push(root);
+    while (q.length !== 0) {
+        const currentLevelSize = q.length;
+        ret.push([]);
+        for (let i = 1; i <= currentLevelSize; ++i) {
+            const node = q.shift();
+            ret[ret.length - 1].push(node.val);
+            if (node.left) q.push(node.left);
+            if (node.right) q.push(node.right);
+        }
     }
-
-    return ans;
+        
+    return ret;
 };
+
--- offer/32-III.js
+++ offer/32-III.js
@@ -1,18 +1,33 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[][]}
- */
 var levelOrder = function(root) {
-    const ans = []
-    const queue = [root]
-    while(queue.length > 0){
-        let current = queue.shift()
+    if (!root) {
+        return [];
     }
+
+    const ans = [];
+    const nodeQueue = [root];
+
+    let isOrderLeft = true;
+
+    while (nodeQueue.length) {
+        let levelList = [];
+        const size = nodeQueue.length;
+        for (let i = 0; i < size; ++i) {
+            const node = nodeQueue.shift();
+            if (isOrderLeft) {
+                levelList.push(node.val);
+            } else {
+                levelList.unshift(node.val);
+            }
+            if (node.left !== null) {
+                nodeQueue.push(node.left);
+            }
+            if (node.right !== null) {
+                nodeQueue.push(node.right);
+            }
+        }            
+        ans.push(levelList);
+        isOrderLeft = !isOrderLeft;
+    }
+
+    return ans;
 };
