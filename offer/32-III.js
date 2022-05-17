5540162386314057959dfaef8e58918851c1dfc0
AsheOne <1029129827@qq.com>
2022-05-18 00:10:26 +0800

    offer day6

---
+++ offer/32-III.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    const ans = []
+    const queue = [root]
+    while(queue.length > 0){
+        let current = queue.shift()
+    }
+};
