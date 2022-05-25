30b0a7322f98ce0b8634a5a0e48bff280a6dcb54
AsheOne <1029129827@qq.com>
2022-05-25 18:16:19 +0800

    offer

---
+++ offer/10-I.js
@@ -0,0 +1,9 @@
+var fib = function(n) {
+    let n1 = 0, n2 = 1, sum;
+    for(let i = 0; i < n; i++){
+        sum = (n1 + n2) % 1000000007;
+        n1 = n2;
+        n2 = sum;
+    }
+    return n1;
+};
