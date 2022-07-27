af5e2851241703bf280e01719921ffeb45cd8e9b
AsheOne <1029129827@qq.com>
2022-07-27 22:01:05 +0800

    vue-challenges

---
+++ vue-challenge/middle/01-raw-family.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive, isReactive，toRaw,markRaw } from "vue"
+
+const state = { count: 1 }
+const reactiveState = reactive(state)
+
+/**
+ * Modify the code so that we can make the output be true.
+*/
+console.log(toRaw(reactiveState) === state)
+
+/**
+ * Modify the code so that we can make the output be false.
+*/
+const info = markRaw({ count: 1 })
+const reactiveInfo = reactive(info)
+
+console.log(isReactive(reactiveInfo))
+</script>
+
+<template>
+  <div>
+    <p>
+      {{ reactiveState.count }}
+    </p>
+  </div>
+</template>
